bc44862604f612014f9523c25251d8ff5073e445
Wisien <wisnia3221@gmail.com>
2019-06-28 15:48:17 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -22,3 +22,16 @@ function prepareBoard(type) {
         $('body').html(board.join(''));
     }
 }*/
+
+for (let i = 0; i < 8; i++) { // Colorize board
+    if (i % 2 == 0) {
+        for (let j = 0; j < 8; j += 2) {
+            $(`#s${i}-${j}`).css("background-color", "white");
+        }
+    }
+    else {
+        for (let j = 1; j < 8; j += 2) {
+            $(`#s${i}-${j}`).css("background-color", "white");
+        }
+    }
+}
