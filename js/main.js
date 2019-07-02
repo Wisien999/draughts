1f12b78bd6c6935fb0bfff29db004a4e9f2e66f6
Wisien <wisnia3221@gmail.com>
2019-07-03 01:44:56 +0200

    Change in stylin board

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-/*$('body').click((e) => {
-    if(e.target.id == "chess" || e.target.id == "draugths") {
-        //alert(e.target +"- next -"+ e.target.id);
-        prepareBoard(e.target.id);
-    }
-
-});
-
-var board = ['<div id="board">'];
-
-function prepareBoard(type) {
-    if(type == "chess") {
-        
-    }
-    else {
-        for(var i = 0; i < 64; i++) {
-           if(i < 16) board.push('<div class="square" id="s', i, '><div class="black_pawn" id="p', i, '></div></div>');
-           else if(i > 48) board.push('<div class="square" id="s', i, '><div class="white_pawn" id="p', i, '></div></div>');
-           else board.push('<div class="square" id="s', i, '></div>');
-        }
-        board.push('</div>');
-        $('body').html(board.join(''));
-    }
-}*/
-
 for (let i = 0; i < 8; i++) { // Colorize board
     if (i % 2 == 0) {
         for (let j = 0; j < 8; j += 2) {
